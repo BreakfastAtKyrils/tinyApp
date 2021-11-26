1b49bfd737a12f7cf884a0ea9ce46b6fcc12e6f2
BreakfastAtKyrils <kyrildr@gmail.com>
2021-11-26 14:09:04 -0500

    implement generateRandomString()

--- express_server.js
+++ express_server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = '';
+  for (let x = 0; x < 6; x++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+console.log(generateRandomString())
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,6 +48,11 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
