e2cd722d314f422b4f2781d60af3fae9b2e7cc8f
BreakfastAtKyrils <kyrildr@gmail.com>
2021-11-27 15:12:13 -0500

    add edit functionality

--- express_server.js
+++ express_server.js
@@ -16,11 +16,9 @@ function generateRandomString() {
   return result;
 }
 
-
-
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2' : "http://www.lighthouselabs.ca",
+  '9sm5xK' : "http://www.google.com"
 };
 
 //this function shows the json object when we navigate to /urls.json
@@ -28,16 +26,26 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//this function renders the urls_new template when we navigate to the link
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//this function create a constant with our urlDatabase object and renders it using
-//the urls_index template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL]
+  const templateVars = { shortURL: shortURL , longURL: longURL  };
+  
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 //this functions is called whenever a user navigates to urls/[any]
@@ -45,23 +53,21 @@ app.get("/urls", (req, res) => {
 // key: shortURL value: shortURL
 // key: longURL: value: urlDatabase.shortURL
 //and passes that object into the res.render function
-app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL
-  const longURL = req.body.longURL
 
-  console.log('longurl is: '+ urlDatabase[shortURL])
+app.post( '/urls/:id', (req, res) => {
+  const shortURL = req.params.id
 
-  const templateVars = { shortURL: shortURL , longURL: longURL /* What goes here? */ };
-  
-  res.render("urls_show", templateVars);
-});
+  console.log(req)
+
+  console.log('you just clicked the edit button')
+  res.redirect(`/urls/${shortURL}`)
+})
+
+
 
 //this function is called when a user navigates to /u/[any]
 //it redirects to the actual website by using the longURL stored in the urlDatabse
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
-});
+
 
 //
 app.post("/urls", (req, res) => {
@@ -72,16 +78,19 @@ app.post("/urls", (req, res) => {
   //test code
   console.log('this is the database after:    ')
   console.log(urlDatabase);
-
-  res.redirect(`/urls/${shortURL}`);
+  res.redirect('/urls')
+  //res.redirect(`/urls/${shortURL}`);
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL]
+  console.log('you just deleted a URL, here is the new database:')
   console.log(urlDatabase)
 
   res.redirect('/urls');
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
